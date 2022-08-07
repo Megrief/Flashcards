a5ff9e1aa9d1ad1960fbb84542cbc28ffb04a5f9
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2022-08-07 09:45:10 +0500

    Add files via upload
    I rewrite the code and add new functionality for the next stage of studying. 
    Now, it asks for unique cards with unique definitions, and tells you if your answer is correct for another card.

--- Main.kt
+++ Main.kt
@@ -1,52 +1,65 @@
 package flashcards
 
+import java.util.Scanner
+val scan = Scanner(System.`in`)
 
 fun main() {
-    println(Text().textArr[0])
-    run { Cards(readln().toInt()) }
-
+    println("Input the number of cards:")
+    val flashCards = Cards(scan.nextInt())
+    flashCards.checkTheAnswer()
 }
 
-class Text {
-    val textArr = arrayOf(
-        "Input the number of cards: ",
-        "Card #",
-        "The definition for card #",
-        "Print the definition of",
-        "Correct!",
-        "Wrong. The right answer is"
-    )
-}
+class Cards(numberOfCards: Int) {
+    private val cards = builder(numberOfCards)
 
-class Cards(private val numberOfCards: Int) {
-    private val terms = MutableList(numberOfCards) { " " }
-    private val definitions = MutableList(numberOfCards) { " " }
-
-    init {
-        creatingCard()
-        for (i in 0 until numberOfCards) {
-            println( if (checkTheAnswer(answer(i), i)) Text().textArr[4] else """
-                ${ Text().textArr[5] } "${ definitions[i] }"
-                 """.trimIndent()
-            )
-        }
-    }
-    private fun checkTheAnswer(answer: String, ind: Int) = answer == definitions[ind]
-
-    private fun creatingCard() {
-        for (i in 0 until numberOfCards) {
-            println(Text().textArr[1] + (i + 1).toString())
-            terms[i] = readln()
-            println(Text().textArr[2] + (i + 1).toString())
-            definitions[i] = readln()
+    private fun builder(size: Int): Map<String, String> {
+        return buildMap {
+            for (num in 1..size) {
+                var term: String
+                var definition: String
+                println("Card #$num:")
+                do {
+                    val key = readln()
+                    if (key !in this.keys) {
+                        term = key
+                        break
+                    } else println("The term \"$key\" already exists. Try again:")
+                } while (true)
+                println("The definition for card #$num:")
+                do {
+                    val value = readln()
+                    if (value !in this.values) {
+                        definition = value
+                        break
+                    } else println("The definition \"$value\" already exists. Try again:")
+                } while (true)
+                put(term, definition)
+            }
         }
     }
 
-    private fun answer(ind: Int): String {
-        println("""
-            ${ Text().textArr[3] } "${ terms[ind] }"
-        """.trimIndent()
-        )
-        return readln()
+    fun checkTheAnswer() {
+        for (term in cards.keys) {
+            println("Print the definition of \"$term\"")
+            val answer = readln()
+            when {
+                answer == cards[term] -> println("Correct!")
+                answer in cards.values && answer != cards[term] ->
+                    println(
+                        "Wrong. The right answer is \"${ cards[term] }\", " +
+                        "but your definition is correct for \"${ getKey(answer) }\"."
+                    )
+                answer !in cards.values ->
+                    println("Wrong. The right answer is \"${ cards[term] }\".")
+            }
+        }
+    }
+
+    private fun getKey(ans: String): String {
+        var res = ""
+        for ((key, value) in cards) {
+            if (value == ans) res = key
+        }
+        return  res
     }
 }
