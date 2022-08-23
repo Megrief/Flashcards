3c8df7dd1f8fcfc5acd71d7386c74b77a61532ad
Eugene Pimenov <109866375+Megrief@users.noreply.github.com>
2022-08-23 10:06:58 +0500

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,45 +1,119 @@
 package flashcards
 
+import java.io.File
+import java.io.FileNotFoundException
 import java.util.Scanner
 val scan = Scanner(System.`in`)
 
 fun main() {
-    println("Input the number of cards:")
-    val flashCards = Cards(scan.nextInt())
-    flashCards.checkTheAnswer()
+    Flashcards()
 }
 
-class Cards(numberOfCards: Int) {
-    private val cards = builder(numberOfCards)
-
-    private fun builder(size: Int): Map<String, String> {
-        return buildMap {
-            for (num in 1..size) {
-                var term: String
-                var definition: String
-                println("Card #$num:")
-                do {
-                    val key = readln()
-                    if (key !in this.keys) {
-                        term = key
-                        break
-                    } else println("The term \"$key\" already exists. Try again:")
-                } while (true)
-                println("The definition for card #$num:")
-                do {
-                    val value = readln()
-                    if (value !in this.values) {
-                        definition = value
-                        break
-                    } else println("The definition \"$value\" already exists. Try again:")
-                } while (true)
-                put(term, definition)
-            }
+class Flashcards {
+    private val cards = mutableMapOf<String, String>()
+    private var stop = false
+    init {
+        while (!stop) {
+            menu()
         }
     }
 
-    fun checkTheAnswer() {
-        for (term in cards.keys) {
+    private fun menu() {
+        println("Input the action (add, remove, import, export, ask, exit):")
+        val input = readln()
+        if (input != "exit") {
+            when (input) {
+                "add" -> adding()
+                "remove" -> removing()
+                "import" -> importing()
+                "export" -> exporting()
+                "ask" -> asking()
+                else -> println("Wrong command!")
+            }
+        } else exit()
+    }
+    private fun adding() {
+        println("The card:")
+        val key: String = readln()
+        try {
+            if (key in cards.keys) throw Exception("Key exists")
+        } catch (e: Exception) {
+            println("The card \"$key\" already exists.")
+            return menu()
+        }
+        println("The definition of the card:")
+        val value: String = readln()
+        try {
+            if (value in cards.values) throw Exception("Value exists")
+        } catch (e: Exception) {
+            println("The definition \"$value\" already exists.")
+            return menu()
+        }
+        cards[key] = value
+        println("The pair (\"$key\":\"$value\") has been added.")
+        return menu()
+    }
+    private fun removing() {
+        println("Which card?")
+        val input = readln()
+        if (input in cards.keys) {
+            cards.remove(input)
+            println("The card has been removed.")
+        } else println("Can't remove \"$input\": there is no such card.")
+        menu()
+    }
+    private fun importing() {
+        println("File name:")
+        try {
+            val file = File(readln())
+            val list: List<String> = file.readLines()
+            val map: Map<String, String> = buildMap {
+                list.forEach {
+                    val li = it.split(':')
+                    this[li[0]] = li[1]
+                }
+            }
+            cards += map
+            println(map.size.toString() + " cards have been loaded." )
+        } catch (e: FileNotFoundException) {
+            println("File not found.")
+        }
+        menu()
+    }
+    private fun exporting() {
+        try {
+            println("File name:")
+            val savedCards = File(readln())
+            val list = buildList {
+                cards.forEach { (key, value) ->
+                    this.add("$key:$value")
+                }
+            }
+            for (ind in list.indices) {
+                if (ind == 0) {
+                    savedCards.writeText("""
+                    ${ list[ind] }
+                    
+                    """.trimIndent()
+                    )
+                } else {
+                    savedCards.appendText("""
+                    ${ list[ind] }
+                    
+                    """.trimIndent()
+                    )
+                }
+            }
+            println(cards.size.toString() + " cards have been saved.")
+        } catch (_: Exception) {
+            println("Something goes wrong!")
+        }
+        menu()
+    }
+    private fun asking() {
+        println("How many times to ask?")
+        repeat(scan.nextInt()) {
+            val term = cards.keys.random()
             println("Print the definition of \"$term\"")
             val answer = readln()
             when {
@@ -53,13 +127,17 @@ class Cards(numberOfCards: Int) {
                     println("Wrong. The right answer is \"${ cards[term] }\".")
             }
         }
+        menu()
     }
-
     private fun getKey(ans: String): String {
         var res = ""
         for ((key, value) in cards) {
             if (value == ans) res = key
         }
-        return  res
+        return res
+    }
+    private fun exit() {
+        println("Bye bye!")
+        stop = true
     }
 }
